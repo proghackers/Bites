eb35a9e2c98d47075fcf7e5b82a8e3a6acfde79f
Raman Guleria <rguleria@expedia.com>
2017-07-31 16:43:40 +0530

    Initalizing the angular app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
+import { FooditemComponent } from './fooditem/fooditem.component';
+import { IngredientComponent } from './ingredient/ingredient.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RestaurantComponent,
+    FooditemComponent,
+    IngredientComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/fooditem/fooditem.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fooditem',
+  templateUrl: './fooditem.component.html',
+  styleUrls: ['./fooditem.component.css']
+})
+export class FooditemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
